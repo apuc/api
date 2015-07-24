f47d0dbe3b1a81ef252b520ea6b7b3d18e69b99f
Kirill Bouko <apuc06@mail.ru>
2015-07-24 12:19:47 +0400

    user crud

---
+++ backend/modules/user/views/user/index.php
@@ -0,0 +1,45 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel backend\modules\user\models\UserSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Пользователи';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="user-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Добавить пользователя', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            //'id',
+            'money',
+            //'cash_id',
+            'email:email',
+            //'password',
+             'created_at:datetime',
+            // 'updated_at',
+            // 'salt',
+             'status',
+             'username',
+            // 'auth_key',
+            // 'photo',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+</div>
