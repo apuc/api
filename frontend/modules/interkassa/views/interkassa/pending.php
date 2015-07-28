c8432465c2a5931580d5261ea13d4035a296fd72
myst1cal <myst1cal@yandex.ru>
2015-07-28 14:52:39 +0300

    Верстка на страницах интеркассы

--- frontend/modules/interkassa/views/interkassa/pending.php
+++ frontend/modules/interkassa/views/interkassa/pending.php
@@ -1,3 +1,18 @@
-<?php
-    echo 'Платеж не проведен, ожидаем подтверждения';
-?>
+<div class="box box-success">
+    <div class="box-header with-border">
+        <h3 class="box-title"></h3>
+    </div>
+    <div class="box-body no-padding">
+        <div class="row">
+            <div class="col-md-12">
+                <div role="form">
+                    <div class="box-body">
+                        <p>
+                            Платеж не проведен, ожидаем подтверждения.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- frontend/modules/interkassa/views/interkassa/success.php
+++ frontend/modules/interkassa/views/interkassa/success.php
@@ -1,3 +1,18 @@
-<?php
-    echo "Платеж проведен, деньги будут зачислены на Ваш счет.";
-?>
+<div class="box box-success">
+    <div class="box-header with-border">
+        <h3 class="box-title">Поздравляем</h3>
+    </div>
+    <div class="box-body no-padding">
+        <div class="row">
+            <div class="col-md-12">
+                <div role="form">
+                    <div class="box-body">
+                        <p>
+                            Платеж проведен, деньги будут зачислены на Ваш счет.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
