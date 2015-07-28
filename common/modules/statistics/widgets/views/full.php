9e04e89c8b7f14d649c7ebbc642595d196d1ebd2
myst1cal <myst1cal@yandex.ru>
2015-07-28 14:16:27 +0300

    вроде робит

--- common/modules/statistics/widgets/views/full.php
+++ common/modules/statistics/widgets/views/full.php
@@ -7,89 +7,75 @@
      */
 ?>
 <div>
-    <div class="col-lg-3">
-        <p>Статистика за сутки:</p>
-
-        <p>Выполнено заданий: <?= $statsForOneDay['done1']; ?></p>
-
-        <p>Пставлено зайков: <?= $statsForOneDay['like1']; ?></p>
-
-        <p>Репостов: <?= $statsForOneDay['repost1']; ?></p>
-
-        <p>Подписчиков: <?= $statsForOneDay['subscriber1']; ?></p>
-
-        <p>Друзей: <?= $statsForOneDay['friend1']; ?></p>
-
-        <p>Комментариев: <?= $statsForOneDay['comment1']; ?></p>
-
-        <p>Опросов: <?= $statsForOneDay['interview1']; ?></p>
+    <table class="table table-bordered table-striped">
+        <thead class="">
+        <td>Статистика Вконтакте</td>
+        <td>Статистика за сутки:</td>
+        <td>Статистика за неделю:</td>
+        <td>Статистика за месяц:</td>
+        <td>Статистика за все время:</td>
+        </thead>
+        <tr>
+            <td>Выполнено заданий:</td>
+            <td><?= $statsForOneDay['done1']; ?></td>
+            <td><?= $statsForSevenDays['done7']; ?></td>
+            <td><?= $statsForOneMonth['done30']; ?></td>
+            <td><?= $statsForAllTime['doneAll']; ?></td>
+        </tr>
+        <tr>
+            <td>Пставлено зайков:</td>
+            <td><?= $statsForOneDay['like1']; ?></td>
+            <td><?= $statsForSevenDays['like7']; ?></td>
+            <td><?= $statsForOneMonth['like30']; ?></td>
+            <td><?= $statsForAllTime['likeAll']; ?></td>
+        </tr>
+        <tr>
+            <td>Репостов:</td>
+            <td><?= $statsForOneDay['repost1']; ?></td>
+            <td><?= $statsForSevenDays['repost7']; ?></td>
+            <td><?= $statsForOneMonth['repost30']; ?></td>
+            <td><?= $statsForAllTime['repostAll']; ?></td>
+        </tr>
+        <tr>
+            <td>Подписчиков:</td>
+            <td><?= $statsForOneDay['subscriber1']; ?></td>
+            <td><?= $statsForSevenDays['subscriber7']; ?></td>
+            <td><?= $statsForOneMonth['subscriber30']; ?></td>
+            <td><?= $statsForAllTime['subscriberAll']; ?></td>
+        </tr>
+        <tr>
+            <td>Друзей:</td>
+            <td><?= $statsForOneDay['friend1']; ?></td>
+            <td><?= $statsForSevenDays['friend7']; ?></td>
+            <td><?= $statsForOneMonth['friend30']; ?></td>
+            <td><?= $statsForAllTime['friendAll']; ?></td>
+        </tr>
+        <tr>
+            <td>Комментариев:</td>
+            <td><?= $statsForOneDay['comment1']; ?></td>
+            <td><?= $statsForSevenDays['comment7']; ?></td>
+            <td><?= $statsForOneMonth['comment30']; ?></td>
+            <td><?= $statsForAllTime['commentAll']; ?></td>
+        </tr>
+        <tr>
+            <td>Опросов:</td>
+            <td><?= $statsForOneDay['interview1']; ?></td>
+            <td><?= $statsForSevenDays['interview7']; ?></td>
+            <td><?= $statsForOneMonth['interview30']; ?></td>
+            <td><?= $statsForAllTime['interviewAll']; ?></td>
+        </tr>
         <?php
-            if (Yii::$app->user->can('administrator'))
-                echo '<p>Сумма, руб:' . $statsForOneDay['sum1'] . '</p>';
-        ?>
-    </div>
-    <div class="col-lg-3">
-        <p>Статистика за неделю:</p>
-
-        <p>Выполнено заданий: <?= $statsForSevenDays['done7']; ?></p>
-
-        <p>Пставлено зайков: <?= $statsForSevenDays['like7']; ?></p>
-
-        <p>Репостов: <?= $statsForSevenDays['repost7']; ?></p>
-
-        <p>Подписчиков: <?= $statsForSevenDays['subscriber7']; ?></p>
-
-        <p>Друзей: <?= $statsForSevenDays['friend7']; ?></p>
-
-        <p>Комментариев: <?= $statsForSevenDays['comment7']; ?></p>
-
-        <p>Опросов: <?= $statsForSevenDays['interview7']; ?></p>
-        <?php
-            if (Yii::$app->user->can('administrator'))
-                echo '<p>Сумма, руб:' . $statsForSevenDays['sum7'] . '</p>';
-        ?>
-    </div>
-    <div class="col-lg-3">
-        <p>Статистика за месяц:</p>
-
-        <p>Выполнено заданий: <?= $statsForOneMonth['done30']; ?></p>
-
-        <p>Пставлено зайков: <?= $statsForOneMonth['like30']; ?></p>
-
-        <p>Репостов: <?= $statsForOneMonth['repost30']; ?></p>
-
-        <p>Подписчиков: <?= $statsForOneMonth['subscriber30']; ?></p>
-
-        <p>Друзей: <?= $statsForOneMonth['friend30']; ?></p>
-
-        <p>Комментариев: <?= $statsForOneMonth['comment30']; ?></p>
-
-        <p>Опросов: <?= $statsForOneMonth['interview30']; ?></p>
-        <?php
-            if (Yii::$app->user->can('administrator'))
-                echo '<p>Сумма, руб:' . $statsForOneMonth['sum30'] . '</p>';
-        ?>
-    </div>
-    <div class="col-lg-3">
-        <p>Статистика за все время:</p>
-
-        <p>Выполнено заданий: <?= $statsForAllTime['doneAll']; ?></p>
-
-        <p>Пставлено зайков: <?= $statsForAllTime['likeAll']; ?></p>
-
-        <p>Репостов: <?= $statsForAllTime['repostAll']; ?></p>
-
-        <p>Подписчиков: <?= $statsForAllTime['subscriberAll']; ?></p>
-
-        <p>Друзей: <?= $statsForAllTime['friendAll']; ?></p>
-
-        <p>Комментариев: <?= $statsForAllTime['commentAll']; ?></p>
-
-        <p>Опросов: <?= $statsForAllTime['interviewAll']; ?></p>
-        <?php
-            if (Yii::$app->user->can('administrator'))
-                echo '<p>Сумма, руб:' . $statsForAllTime['sumAll'] . '</p>';
-        ?>
-    </div>
-
+            if (Yii::$app->user->can('administrator')):
+                ?>
+                <tr>
+                    <td>
+                        Сумма, руб:
+                    </td>
+                    <td><?= $statsForOneDay['sum1'] ?></td>
+                    <td><?= $statsForSevenDays['sum7'] ?>]</td>
+                    <td><?= $statsForOneMonth['sum30'] ?></td>
+                    <td><?= $statsForAllTime['sumAll'] ?></td>
+                </tr>
+            <?php endif; ?>
+    </table>
 </div>
